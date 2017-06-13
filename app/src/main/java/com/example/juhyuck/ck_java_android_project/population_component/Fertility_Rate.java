e7aa00e981a4743904f42cdda64f23309c81a4f8
choijuhyuck <dkssuddl91@naver.com>
2017-06-14 02:22:28 +0900

    listview로 만들어야한다.

--- app/src/main/java/com/example/juhyuck/ck_java_android_project/population_component/Fertility_Rate.java
+++ app/src/main/java/com/example/juhyuck/ck_java_android_project/population_component/Fertility_Rate.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.example.juhyuck.ck_java_android_project.R;
 
 public class Fertility_Rate extends AppCompatActivity {
-
+//ㅇㄹㅇㄹ
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
