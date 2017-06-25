e9e740aae11d0a19d7ac6a9102043c6cc520287b
choijuhyuck <dkssuddl91@naver.com>
2017-06-26 01:39:25 +0900

    listview 한개 완성

--- app/src/main/java/com/example/juhyuck/ck_java_android_project/listview_item_adpater/ListViewAdapter.java
+++ app/src/main/java/com/example/juhyuck/ck_java_android_project/listview_item_adpater/ListViewAdapter.java
@@ -1,13 +1,73 @@
 package com.example.juhyuck.ck_java_android_project.listview_item_adpater;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.juhyuck.ck_java_android_project.R;
+
+import java.util.ArrayList;
+
 /**
  * Created by juhyuck on 2017-06-25.
  */
-/*
+
 public class ListViewAdapter extends BaseAdapter {
-        private ArrayList<ListViewItem>
+
+    private ArrayList<ListViewItem> arraylist = new ArrayList<ListViewItem>();
+
+    public ListViewAdapter(){
+
+    }
+
+    @Override
+    public int getCount(){
+        return arraylist.size();
+    }
+
+    @Override
+    public Object getItem(int i){
+        return arraylist.get(i);
+    }
+
+    @Override
+    public long getItemId(int i){
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup){
+        Context context = viewGroup.getContext();
+
+        if(view==null){
+            LayoutInflater inflater =(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.listviewitem,viewGroup,false);
+        }
+
+        TextView text1 = (TextView)view.findViewById(R.id.year_tv);
+        TextView text2 = (TextView)view.findViewById(R.id.data_tv);
+
+        ListViewItem listViewItem = arraylist.get(i);
+
+        text1.setText(listViewItem.getText1());
+        text2.setText(listViewItem.getText2());
+
+        return view;
+    }
+    public void addItem(String text1, String text2){
+        ListViewItem listViewItem = new ListViewItem();
+        listViewItem.setText1(text1);
+        listViewItem.setText2(text2);
+
+        arraylist.add(listViewItem);
+    }
 
 
 
 
-}*/
+
+
+}
